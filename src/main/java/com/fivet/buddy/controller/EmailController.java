21baad064b0f36107bee82c03ed2665614afff79
Jung-taekku-dev <xorrn93@gmail.com>
2023-04-03 21:32:43 +0900

    Refactor

--- src/main/java/com/fivet/buddy/controller/EmailController.java
+++ src/main/java/com/fivet/buddy/controller/EmailController.java
@@ -1,10 +1,13 @@
 package com.fivet.buddy.controller;
 
-import com.fivet.buddy.dto.InviteDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.fivet.buddy.services.InviteService;
+import com.fivet.buddy.services.MemberService;
 import com.fivet.buddy.util.EmailUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import netscape.javascript.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +23,9 @@ public class EmailController {
     @Autowired
     private InviteService inviteService;
 
+    @Autowired
+    private MemberService ms;
+
     private final EmailUtil emailUtil;
 
     // ExceptionHandler
@@ -33,7 +39,7 @@ public class EmailController {
     @PostMapping("sendEmail")
     public Map<String, Object> sendEmail(@RequestBody Map<String, Object> params){
         return emailUtil.sendEmail( (String) params.get("userId")
-                , (String) params.get("subject")
+                , (String) params.get("subject").toString()
                 , (String) params.get("body")
         );
     }
@@ -42,8 +48,7 @@ public class EmailController {
     @ResponseBody
     @PostMapping("emailProve")
     public String emailProve() throws  Exception{
-        String ranVal = createEmailKey();
-        return ranVal;
+        return createEmailKey();
     }
 
     // 이메일 인증코드 (숫자 6)
@@ -73,8 +78,7 @@ public class EmailController {
     @ResponseBody
     @PostMapping("inviteProve")
     public String inviteProve() throws Exception{
-        String ranVal = inviteCode();
-        return ranVal;
+        return inviteCode();
     }
 
     // 숫자+영문 10자리 생성
@@ -92,4 +96,37 @@ public class EmailController {
         System.out.println(key);
         return key.toString();
     }
+
+    // 임시비밀번호 생성
+    public String getTempPW(){
+        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        String str = "";
+
+        // 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
+        int idx = 0;
+        for (int i = 0; i < 10; i++) {
+            idx = (int) (charSet.length * Math.random());
+            str += charSet[idx];
+        }
+        return str;
+    }
+    // 임시 비밀번호 리턴
+    @ResponseBody
+    @PostMapping("returnTempPW")
+    public String returnTempPW() throws  Exception{
+        return getTempPW();
+    }
+    // 임시비밀번호 발송 및 업데이트
+    @PostMapping("sendPW")
+    public Map<String, Object> sendPW(@RequestBody Map<String, Object> params){
+//        ms.updateTempPW((String) params.get("body"),(String) params.get("userId"));
+        System.out.println((String)params.get("userId"));
+        return emailUtil.sendEmail( (String)params.get("userId")
+                , (String)params.get("subject")
+                , (String)params.get("body")
+        );
+    }
+
 }
