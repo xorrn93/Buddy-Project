e34dfea1f77b57ac6b9f97d1cb9cc76898c3cfc6
Jung-taekku-dev <xorrn93@gmail.com>
2023-04-03 16:18:15 +0900

    Refactor code

--- src/main/java/com/fivet/buddy/BuddyApplication.java
+++ src/main/java/com/fivet/buddy/BuddyApplication.java
@@ -2,12 +2,19 @@ package com.fivet.buddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BuddyApplication {
+public class BuddyApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BuddyApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(BuddyApplication.class);
     }
 
 }
